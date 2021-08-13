c7c4606ad13376b242b21f2a04c9c4b4c3083943
Bit-byte <webinc001@gmail.com>
2021-08-13 23:08:19 +0600

    Added styles for the header area

--- pages/map.js
+++ pages/map.js
@@ -1,7 +1,33 @@
 import styles from '../styles/Map.module.css'
+import Image from 'next/image'
 function Map() {
     return (
-        <div className={styles.container}>Map</div>
+        <div className={styles.container}>
+            <header className={`${styles.nav} cf`} >
+                <section className={styles.logo} >
+                    <Image
+                        src="/logo.png"
+                        alt="A logo of an imaginary company"
+                        layout='fill'
+                        objectFit='contain'
+                    />
+                </section>
+                <nav className={styles.nav_menu} >
+                    <ul className={`${styles.menu} cf`}  >
+                        <li><a href="#">Dashboard</a></li>
+                        <li><a href="#">Project</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Download</a></li>
+                    </ul>
+                </nav>
+                <section className={styles.avatar_area} >
+                    fsdfsdf
+                </section>
+            </header>
+            <content className={styles.content} >
+
+            </content>
+        </div>
     )
 }
 
